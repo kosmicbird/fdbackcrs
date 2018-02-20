931fb8fc4e0d225160fae83557f0d344de9ecfe6
kosmicbird <kosmicbird@gmail.com>
2018-02-19 23:35:47 -0600

    Authentication Routes

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = require('bluebird');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/React'/*, {
+mongoose.connect(process.env.MONGODB_URI/*, {
     useMongoClient: true
 }*/);
 
