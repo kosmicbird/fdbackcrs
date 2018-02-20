e0fe639cbef5565aa21933889a2eeef0024b8de3
kosmicbird <kosmicbird@gmail.com>
2018-02-20 00:38:28 -0600

    Authentication Routes

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT,  "0.0.0.0", () => {
     console.log(`Server started on port ${PORT}`);
 });
