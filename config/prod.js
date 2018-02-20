49e7f9e519bcf15e19063d37a5a08e9d5aa78062
kosmicbird <kosmicbird@gmail.com>
2018-02-19 21:41:55 -0600

    Authentication Routes

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,9 @@
+let cookie = "process.env.COOKIE_KEY"
+
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    cookieKey: process.env.COOKIE_KEY,
+    cookieKey: cookie,
     MONGODB_URI: process.env.MONGODB_URI
 
 };
